b43feed3a9cd55e76b32fb8ee3ab74c004147512
androimaster <androimaster@gmail.com>
2023-08-29 14:40:06 +0900

    7장연습문제 수정

--- src/main/java/chap07/Test1.java
+++ src/main/java/chap07/Test1.java
@@ -1,17 +1,16 @@
 package chap07;
 
-import java.util.Scanner;
-
 public class Test1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("정수를 입력하세요 : ");
-        int num = in.nextInt();
-        if(num > 0) {
-            System.out.printf("%d는 양수입니다.", num);
-        }
-        else {
-            System.out.printf("%d는 음수입니다.", num);
+        Integer[] numbers = {-1, 7, 4, -8};
+        
+        for(int number : numbers) {
+            if(number > 0) {
+                System.out.printf("%d는 양수입니다.\n", number);
+            }
+            else {
+                System.out.printf("%d는 음수입니다.\n", number);
+            }
         }
     }
 }
